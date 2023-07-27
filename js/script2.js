8159e744f3fe9ce7b6df493599ad24bef608aeb3
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-07-27 07:46:07 +0000

    finish 4-5

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
+  // ↑クリックされた時の処理
+  // 要素.classList.toggle('クラス名');
+  // クリックする度につけたり外したりするからdark-themeの追加と削除を交互に行えるようにtoggle
 
   if (btn.textContent === 'ダークモードにする'){
     btn.textContent = 'ライトモードにする';
