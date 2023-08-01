2f043f2f43115f6783377b797e974bc3487c2400
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-08-01 01:37:57 +0000

    finish 4-15

--- js/script5.js
+++ js/script5.js
@@ -2,14 +2,15 @@ const getScrollPercent = () => {
   // console.log('スクロールされました');
   // スクロール量
   const scrolled = window.scrolly;
-  // console.log(`${scrolled} スクロールされました`);
+  console.log(`${scrolled} スクロールされました`);
 
   // ページ全体の高さ
   const pageHeight = document.documentElement.scrollHeight;
 
   // 表示領域の高さ
   const viewHeight = document.documentElement.clientHeight;
-
+  console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
+  
   // スクロールされた割合
   const percentage = scrolled / (pageHeight - viewHeight) * 100;
 
