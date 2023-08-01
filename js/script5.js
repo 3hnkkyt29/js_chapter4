9189128d2b617670c8dc73aef777626e5b1695d3
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-08-01 01:46:55 +0000

    middle of 4-16

--- js/script5.js
+++ js/script5.js
@@ -4,15 +4,27 @@ const getScrollPercent = () => {
   const scrolled = window.scrolly;
   console.log(`${scrolled} スクロールされました`);
 
+  // 4-15
   // ページ全体の高さ
   const pageHeight = document.documentElement.scrollHeight;
 
   // 表示領域の高さ
   const viewHeight = document.documentElement.clientHeight;
   console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
-  
+
+  // 4-16
   // スクロールされた割合
   const percentage = scrolled / (pageHeight - viewHeight) * 100;
+  /*
+  + 足し算
+  - 引き算
+  * 掛け算
+  / 割り算
+  % 割り算のあまりを計算
+  ** ⚪︎乗を計算
+
+  */
+
 
   // プログレスバーに幅を指定
   document.querySelector('#bar').style.width = `${percentage}%`;
