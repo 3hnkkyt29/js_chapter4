a0c9f63180b887f3868a901199d5157cf3c82d4c
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-08-02 08:34:02 +0000

    fix 4-16

--- js/script5.js
+++ js/script5.js
@@ -1,53 +1,33 @@
-// const getScrollPercent = () => {
-//   // console.log('スクロールされました');
-//   // スクロール量
-//   const scrolled = window.scrolly;
-//   // console.log(`${scrolled} スクロールされました`);
-
-//   // 4-15
-//   // ページ全体の高さ
-//   const pageHeight = document.documentElement.scrollHeight;
-
-//   // 表示領域の高さ
-//   const viewHeight = document.documentElement.clientHeight;
-//   // console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
-
-//   // 4-16
-//   // スクロールされた割合
-//   const percentage = scrolled / (pageHeight - viewHeight) * 100;
-//   /*
-//   + 足し算
-//   - 引き算
-//   * 掛け算
-//   / 割り算
-//   % 割り算のあまりを計算
-//   ** ⚪︎乗を計算
-//   */
-
-//   // プログレスバーに幅を指定
-//   document.querySelector('#bar').style.width = `${percentage}%`;
-// }
-
-// window.addEventListener('scroll', getScrollPercent);
-
 const getScrollPercent = () => {
+  // console.log('スクロールされました');
   // スクロール量
   const scrolled = window.scrollY;
-  //console.log(`${scrolled} スクロールされました`);
+  // スクロール量は「scrollY」で取得できる（Yは大文字注意！）
+  // console.log(`${scrolled} スクロールされました`);
 
+  // 4-15
   // ページ全体の高さ
   const pageHeight = document.documentElement.scrollHeight;
 
   // 表示領域の高さ
   const viewHeight = document.documentElement.clientHeight;
-  //console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
+  // console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
 
+  // 4-16
   // スクロールされた割合
   const percentage = scrolled / (pageHeight - viewHeight) * 100;
-  //console.log(percentage);
-  
+  /*
+  + 足し算
+  - 引き算
+  * 掛け算
+  / 割り算
+  % 割り算のあまりを計算
+  ** ⚪︎乗を計算
+  */
+
   // プログレスバーに幅を指定
   document.querySelector('#bar').style.width = `${percentage}%`;
 }
 
 window.addEventListener('scroll', getScrollPercent);
+
